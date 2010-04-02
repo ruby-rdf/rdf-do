9778ed623488324e4ab848089df8e2595128d173
Ben Lavender <blavender@gmail.com>
2010-04-02 07:02:54 +0200

    Update sqlite3 to new adapter system

--- lib/rdf/do/adapters/sqlite3.rb
+++ lib/rdf/do/adapters/sqlite3.rb
@@ -7,75 +7,32 @@ module RDF::DataObjects
   module Adapters
     class Sqlite3
 
-      def initialize(db)
-        @db = db
+      def self.migrate?(do_repository, opts = {})
+        do_repository.exec('CREATE TABLE quads (`subject` varchar(255), `predicate` varchar(255), `object` varchar(255), `context` varchar(255), UNIQUE (`subject`, `predicate`, `object`, `context`))')
+        begin do_repository.exec('CREATE INDEX `quads_context_index` ON `quads` (`context`)') rescue nil end
+        begin do_repository.exec('CREATE INDEX `quads_object_index` ON `quads` (`object`)') rescue nil end
+        begin do_repository.exec('CREATE INDEX `quads_predicate_index` ON `quads` (`predicate`)') rescue nil end
+        begin do_repository.exec('CREATE INDEX `quads_subject_index` ON `quads` (`subject`)') rescue nil end
       end
 
-      def migrate?(opts = {})
-        exec('CREATE TABLE quads (`subject` varchar(255), `predicate` varchar(255), `object` varchar(255), `context` varchar(255), UNIQUE (`subject`, `predicate`, `object`, `context`))')
-        begin exec('CREATE INDEX `quads_context_index` ON `quads` (`context`)') rescue nil end
-        begin exec('CREATE INDEX `quads_object_index` ON `quads` (`object`)') rescue nil end
-        begin exec('CREATE INDEX `quads_predicate_index` ON `quads` (`predicate`)') rescue nil end
-        begin exec('CREATE INDEX `quads_subject_index` ON `quads` (`subject`)') rescue nil end
+      def self.count_sql
+        'select count(*) from `quads`'
       end
 
-      def count
-        result = result('select count(*) from `quads`')
-        result.next!
-        result.values.first
+      def self.insert_sql
+        'REPLACE INTO `quads` (subject, predicate, object, context) VALUES (?, ?, ?, ?)'
       end
 
-      def insert(*statements)
-        query = "REPLACE INTO `quads` (subject, predicate, object, context) VALUES (?, ?, ?, ?)"
-        statements.each do |s|
-          exec(query,serialize(s. subject),serialize(s.predicate), serialize(s.object), serialize(s.context)) 
-        end
-        
+      def self.delete_sql
+        'DELETE FROM `quads` where (subject = ? AND predicate = ? AND object = ? AND context = ?)'
       end
 
-      def delete(*statements)
-        query = "DELETE FROM `quads` where (subject = ? AND predicate = ? AND object = ? AND context = ?)"
-        statements.each do |s|
-          exec(query,serialize(s. subject),serialize(s.predicate), serialize(s.object), serialize(s.context)) 
-        end
-      end
-
-      def query(query_stuff)
+      def self.query
         # only deal with hash here
       end
 
-      def each_subject(&block)
-        if block_given?
-        else
-          self.enum_subjects
-        end
-      end
-
-      def each(&block)
-        reader = result('select * from quads')
-        while reader.next!
-          block.call(RDF::Statement.new(
-                      :subject   => unserialize(reader.values[0]),
-                      :predicate => unserialize(reader.values[1]),
-                      :object    => unserialize(reader.values[2]),
-                      :context   => unserialize(reader.values[3])))
-        end
-      end
-
-      def serialize(value)
-        value.nil? ? 'nil' : RDF::NTriples::Writer.serialize(value)
-      end
-
-      def unserialize(value)
-        value == 'nil' ? nil : RDF::NTriples::Reader.unserialize(value)
-      end
-      
-      def exec(sql, *args)
-        @db.create_command(sql).execute_non_query(*args)
-      end
-
-      def result(sql, *args)
-        @db.create_command(sql).execute_reader(*args)
+      def self.each_sql
+        'select * from quads'
       end
 
     end
