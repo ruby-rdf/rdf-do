594fe8b9420503b5130a1814a32dc570f06df6ef
Ben Lavender <blavender@gmail.com>
2010-04-02 06:56:30 +0200

    Rework most of the logic from the adapter to the repository (sqlite still needs doing)

--- lib/rdf/do.rb
+++ lib/rdf/do.rb
@@ -23,8 +23,8 @@ module RDF
           warn "Unable to find adapter '#{adapter}'."
           raise e
         end
-        @adapter = RDF::DataObjects::Adapters::const_get(adapter.to_s.capitalize).new(@db)
-        @adapter.migrate?
+        @adapter = RDF::DataObjects::Adapters::const_get(adapter.to_s.capitalize)
+        @adapter.migrate? self
       end
 
       def dispose
@@ -37,40 +37,87 @@ module RDF
         @adapter = nil
       end
 
-      def count
-        @adapter.count
-      end
-
       def empty?
         count == 0
-      end
-
-      def insert(*statements)
-        @adapter.insert *statements
       end
 
       def insert_statement(statement)
         insert *[statement]
       end
 
-      def delete(*statements)
-        @adapter.delete *statements
-      end
-
       def delete_statement(statement)
         delete *[statement]
       end
 
+      #def each(&block)
+      #  if block_given?
+      #    @adapter.each &block
+      #  else
+      #    enum_statements(@adapter, :each, @adapter)
+      #  end
+      #end
+
       def each(&block)
         if block_given?
-          @adapter.each &block
+          reader = result(@adapter.each_sql)
+          while reader.next!
+            block.call(RDF::Statement.new(
+                      :subject   => unserialize(reader.values[0]),
+                      :predicate => unserialize(reader.values[1]),
+                      :object    => unserialize(reader.values[2]),
+                      :context   => unserialize(reader.values[3])))
+          end
         else
-          enum_statements(@adapter, :each, @adapter)
+          ::Enumerable::Enumerator.new(self,:each)
+          #enum_statements(@adapter, :each, @adapter)
         end
       end
 
-      def enum_statements
-        ::Enumerable::Enumerator.new(@adapter, :each)
+      def serialize(value)
+        value.nil? ? 'nil' : RDF::NTriples::Writer.serialize(value)
+      end
+
+      def unserialize(value)
+        value == 'nil' ? nil : RDF::NTriples::Reader.unserialize(value)
+      end
+      
+      def exec(sql, *args)
+        @db.create_command(sql).execute_non_query(*args)
+      end
+
+      def result(sql, *args)
+        @db.create_command(sql).execute_reader(*args)
+      end
+
+      #def each_subject(&block)
+      #  if block_given?
+      #  else
+      #    self.enum_subjects
+      #  end
+      #end
+
+      def insert(*statements)
+        query = @adapter.insert_sql
+        statements.each do |s|
+          exec(query,serialize(s. subject),serialize(s.predicate), serialize(s.object), serialize(s.context)) 
+        end
+      end
+
+      def delete(*statements)
+        query = @adapter.delete_sql
+        statements.each do |s|
+          exec(query,serialize(s. subject),serialize(s.predicate), serialize(s.object), serialize(s.context)) 
+        end
+      end
+
+      #def query(query_stuff)
+        # only deal with hash here
+      #end
+
+      def count
+        result = result(@adapter.count_sql)
+        result.next!
+        result.values.first
       end
 
     end
