e84b85fc86ab442c287d6536c748b9d54e10f7f8
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-01-09 19:07:37 -0800

    Updated to use #query_pattern with full support for SPARQL-based queries.

--- lib/rdf/do/adapters/defaults.rb
+++ lib/rdf/do/adapters/defaults.rb
@@ -44,6 +44,10 @@ module RDF::DataObjects
       #
       # This is meant to be called by RDF::DataObjects::Repository.
       #
+      # Supports symbols and `RDF::Query::Variable` values as a wild-card for a non-nil value.
+      #
+      # Supports `false` for a specifically nil value representing the default context.
+      #
       # @example
       #     adapter.query(repository, :predicate => predicate)
       # @return [DataObjects::Result]
@@ -53,12 +57,26 @@ module RDF::DataObjects
         params = []
         [:subject, :predicate, :object, :context].each do |resource|
           unless hash[resource].nil?
-            conditions << "#{resource.to_s} = ?"
+            if resource == :context
+              case hash[resource]
+              when Symbol, RDF::Query::Variable
+                conditions << "#{resource.to_s} != 'nil'"
+                next
+              when false
+                conditions << "#{resource.to_s} = 'nil'"
+                next
+              else
+                conditions << "#{resource.to_s} = ?"
+              end
+            else
+              conditions << "#{resource.to_s} = ?"
+            end
             params     << repository.serialize(hash[resource])
           end
         end
         where = conditions.empty? ? "" : "WHERE "
         where << conditions.join(' AND ')
+        puts "query: #{where.inspect}, #{params.inspect}"
         repository.result('select * from quads ' + where, *params)
       end
 
